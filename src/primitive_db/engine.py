11559e7e9622d38a7a32b1154beee16493d747bc
Jura.me <m@jura.me>
2025-11-14 22:47:03 +0300

    + файлы core.py, utils.py, добавлена функция print_help

--- src/primitive_db/engine.py
+++ src/primitive_db/engine.py
@@ -16,3 +16,18 @@ def welcome():
             print("<command> help - справочная информация")
         else:
             print(f"Неизвестная команда: {command}")
+
+
+
+def print_help():
+    """Prints the help message for the current mode."""
+
+    print("\n***Процесс работы с таблицей***")
+    print("Функции:")
+    print("<command> create_table <имя_таблицы> <столбец1:тип> .. - создать таблицу")
+    print("<command> list_tables - показать список всех таблиц")
+    print("<command> drop_table <имя_таблицы> - удалить таблицу")
+
+    print("\nОбщие команды:")
+    print("<command> exit - выход из программы")
+    print("<command> help - справочная информация\n")
